8c07c114dc7503f65998360e366d4ae153c6b1a7
QueenTesa Fuggett <qfuggett@umich.edu>
2021-02-20 00:55:25 -0500

    commit tree d8469a07ac9067f50602c5d8615eaa6c766dc3c7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
   
+  get '/' do
+    redirect '/recipes'
+  end
+
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
@@ -12,17 +16,36 @@ class ApplicationController < Sinatra::Base
   get '/recipes/new' do
     erb :new
   end
+
+  post '/recipes' do 
+    @recipe = Recipe.create(name: params[:name],
+      ingredients: params[:ingredients],
+      cook_time: params[:cook_time])
+      redirect "/recipes/#{@recipe.id}"
+  end
   
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
   
-  get 'recipes/:id/edit' do
-    @recipe = Recipe.find_by_id(params[:id])
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(id: params[:id])
     erb :edit
-  end
+  end 
   
-  
+  patch '/recipes/:id' do
+    Recipe.find_by(id: params[:id]).tap do |recipe|
+      @recipe.update(name: params[:name],
+        ingredients: params[:ingredients],
+        cook_time: params[:cook_time])
+    end
+    redirect "/recipes/#{@recipe.id}"
+  end 
+
+  delete '/recipes/:id' do 
+    Recipe.find_by(id: params[:id]).destroy
+    redirect '/recipes'
+  end 
   
 end
